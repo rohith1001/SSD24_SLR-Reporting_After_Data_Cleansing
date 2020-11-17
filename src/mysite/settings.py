a507f0ed27fd221dacc2e214f40ff1349d061add
rohith1001 <rohith.sharma.1995@gmail.com>
2020-11-17 19:38:50 +0530

    added graphs to UI

--- src/mysite/settings.py
+++ src/mysite/settings.py
@@ -118,9 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "images"),
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'images'),
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
